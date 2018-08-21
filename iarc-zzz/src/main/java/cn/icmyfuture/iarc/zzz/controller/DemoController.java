17442abc46e879587dea782111f8b99b2fe2ba68
icmyfuture <icmyfuture@163.com>
2018-08-21 18:28:04 +0800

    updated

--- iarc-zzz/src/main/java/cn/icmyfuture/iarc/zzz/controller/DemoController.java
+++ iarc-zzz/src/main/java/cn/icmyfuture/iarc/zzz/controller/DemoController.java
@@ -1,5 +1,7 @@
 package cn.icmyfuture.iarc.zzz.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
+    private static final String pubGCounterName = "iarc-zzz.demo_index.count";
+
+    @Autowired
+    private CounterService counterService;
+
     @CrossOrigin(origins = "*")
     @GetMapping("index")
     public int pubG() {
+        counterService.increment(pubGCounterName);
         return 4004;
     }
 }
---
+++ iarc-zzz/src/main/java/cn/icmyfuture/iarc/zzz/indicator/RabbitMQHealthIndicator.java
@@ -0,0 +1,13 @@
+package cn.icmyfuture.iarc.zzz.indicator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.up().build();
+    }
+}
